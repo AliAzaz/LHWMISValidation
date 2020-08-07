11c07825dea2433adbe8d3785bf88c70b5245c88
javed.khan <javedak09@gmail.com>
2020-08-07 14:58:20 +0500

    Merge branch 'LHWMIS_2020' of D:\LHWMISValidation with conflicts.

--- app/src/main/java/com/irfansyed/umeedenau/validation/Form3SectionB.java
+++ app/src/main/java/com/irfansyed/umeedenau/validation/Form3SectionB.java
@@ -3,186 +3,81 @@ package com.irfansyed.umeedenau.validation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.irfansyed.umeedenau.validation.databinding.Form3sectionbBinding;
-import com.validatorcrawler.aliazaz.Clear;
 
 import java.util.HashMap;
 
+import utils.ClearAllcontrol;
 import utils.GeneratorClass;
 import utils.ValidatorClass;
 
 
-public class Form3SectionB extends AppCompatActivity implements View.OnClickListener {
+public class Form3SectionB extends AppCompatActivity implements View.OnClickListener, RadioButton.OnCheckedChangeListener {
 
 
     //region Initialization
     Form3sectionbBinding bin;
     private int PhotoSerial;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bin = DataBindingUtil.setContentView(this, R.layout.form3sectionb);
-
-        setupSkips();
-
         PhotoSerial = 0;
 
         bin.btnNext.setOnClickListener(this);
         bin.btnSnap.setOnClickListener(this);
 
-    }
+        bin.lhwf3b11.setOnCheckedChangeListener(this);
+        bin.lhwf3b12.setOnCheckedChangeListener(this);
+
+        bin.lhwf3b21.setOnCheckedChangeListener(this);
+        bin.lhwf3b22.setOnCheckedChangeListener(this);
 
 
-    private void setupSkips() {
-
-        bin.lhwf3b1.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (bin.lhwf3b12.isChecked()) {
-
-                bin.cvlhwf3b2.setVisibility(View.GONE);
-                bin.cvlhwf3b3.setVisibility(View.GONE);
-                bin.cvlhwf3b4a.setVisibility(View.GONE);
-                bin.cvlhwf3b4b.setVisibility(View.GONE);
-                bin.cvlhwf3b4c.setVisibility(View.GONE);
-
-                bin.cvlhwf3b6.setVisibility(View.VISIBLE);
-
-                Clear.clearAllFields(bin.cvlhwf3b2);
-                Clear.clearAllFields(bin.cvlhwf3b3);
-                Clear.clearAllFields(bin.cvlhwf3b4a);
-                Clear.clearAllFields(bin.cvlhwf3b4b);
-                Clear.clearAllFields(bin.cvlhwf3b4c);
-
-                bin.cvlhwf3b5b.setVisibility(View.VISIBLE);
-
-            } else if (bin.lhwf3b11.isChecked()) {
-
-                bin.cvlhwf3b2.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b3.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b4a.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b4b.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b4c.setVisibility(View.VISIBLE);
-
-                bin.cvlhwf3b6.setVisibility(View.VISIBLE);
-
-            }
-        }));
-
-
-        bin.lhwf3b2.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (bin.lhwf3b21.isChecked() && bin.lhwf3b11.isChecked()) {
-
-                bin.cvlhwf3b3.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b4a.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b4b.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b4c.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b5.setVisibility(View.VISIBLE);
-                bin.cvlhwf3b5b.setVisibility(View.VISIBLE);
-
-                bin.cvlhwf3b6.setVisibility(View.GONE);
-
-            } else if (bin.lhwf3b22.isChecked()) {
-
-                bin.cvlhwf3b3.setVisibility(View.GONE);
-                bin.cvlhwf3b4a.setVisibility(View.GONE);
-                bin.cvlhwf3b4b.setVisibility(View.GONE);
-                bin.cvlhwf3b4c.setVisibility(View.GONE);
-                bin.cvlhwf3b5.setVisibility(View.GONE);
-                bin.cvlhwf3b5b.setVisibility(View.GONE);
-
-                Clear.clearAllFields(bin.cvlhwf3b3);
-                Clear.clearAllFields(bin.cvlhwf3b4a);
-                Clear.clearAllFields(bin.cvlhwf3b4b);
-                Clear.clearAllFields(bin.cvlhwf3b4c);
-                Clear.clearAllFields(bin.cvlhwf3b5);
-                Clear.clearAllFields(bin.cvlhwf3b5b);
-
-                bin.cvlhwf3b6.setVisibility(View.VISIBLE);
-            }
-        }));
-
-
-        bin.lhwf3b5.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (bin.lhwf3b52.isChecked()) {
-                bin.cvlhwf3b5b.setVisibility(View.VISIBLE);
-
-                Clear.clearAllFields(bin.cvlhwf3b6);
-                bin.cvlhwf3b6.setVisibility(View.GONE);
-
-            } else if (bin.lhwf3b51.isChecked()) {
-                bin.cvlhwf3b5b.setVisibility(View.GONE);
-                Clear.clearAllFields(bin.cvlhwf3b5b);
-
-                bin.cvlhwf3b6.setVisibility(View.VISIBLE);
-            }
-        }));
-
-
-        bin.lhwf3b5b.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (bin.lhwf3b5b96.isChecked()) {
-                bin.lhwf3b5b96x.setVisibility(View.VISIBLE);
-            } else {
-                bin.lhwf3b5b96x.setVisibility(View.GONE);
-                Clear.clearAllFields(bin.lhwf3b5b96x);
-            }
-        }));
-
+        bin.lhwf3b51.setOnCheckedChangeListener(this);
+        bin.lhwf3b52.setOnCheckedChangeListener(this);
 
     }
 
 
     @Override
     public void onClick(View view) {
-
         if (view.getId() == R.id.btn_snap) {
-
             Intent intent = new Intent(this, TakePhoto.class);
 
             //   intent.putExtra("picID", "901001" + "_" + "A-0001-001" + "_" + PhotoSerial + "_");
             //   intent.putExtra("childName", "Hassan");
+
 // TODO: add identification information
-            intent.putExtra("picID", Global.LhwSection_id + "_" + PhotoSerial);
-            intent.putExtra("childName", "VHC SR");
-            intent.putExtra("picView", "Sect_VHC_SR".toUpperCase());
+            intent.putExtra("picID", Global.LhwHH_id + "_" + Global.LhwSection_id + "_" + PhotoSerial);
+            intent.putExtra("childName", "Village Health Committee ");
+
+
+            intent.putExtra("picView", "Sect_3B".toUpperCase());
             startActivityForResult(intent, 1); // Activity is started with requestCode 1 = Front
 
         }
-
-
-        if (view.getId() == R.id.btnNext) {
-
-            if (!formValidation()) {
-                return;
-            }
-
-            insert_data();
-            Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
-
-            finish();
+        if (!formValidation()) {
+            return;
         }
+
+        insert_data();
+        Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 
 
     private boolean formValidation() {
-
-        if (!ValidatorClass.EmptyCheckingContainer(this, bin.SectionB)) {
-            return false;
-        }
-
-        if (!bin.lhwf3bphoto.getText().toString().equals("")) {
-
-        } else {
-            Toast.makeText(this, "Please take photo", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-
-        return true;
+        return ValidatorClass.EmptyCheckingContainer(this, bin.SectionB);
     }
 
 
@@ -195,9 +90,7 @@ public class Form3SectionB extends AppCompatActivity implements View.OnClickList
         Has_Map.put("Status", "0");
 
 
-        //GeneratorClass.Insert_table(bin.SectionB, true);
-
-        GeneratorClass.getContainerJSON(bin.SectionB, true);
+        GeneratorClass.Insert_table(bin.SectionB, true);
         GeneratorClass.inert_db("TableF3SectionB", this, Has_Map);
         GeneratorClass.LHWSectionUpdateCOunt("LHWOfficeVHCCount", Global.LhwSection_id, this);
 
@@ -205,7 +98,7 @@ public class Form3SectionB extends AppCompatActivity implements View.OnClickList
     }
 
 
-    /*@Override
+    @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
 
@@ -249,6 +142,7 @@ public class Form3SectionB extends AppCompatActivity implements View.OnClickList
 
             if (bin.lhwf3b22.isChecked()) {
 
+
                 ClearAllcontrol.ClearAll(bin.LvLhwf3b6);
 
             }
@@ -256,26 +150,21 @@ public class Form3SectionB extends AppCompatActivity implements View.OnClickList
         }
 
 
-    }*/
-
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
         // check if the request code is same as what is passed  here it is 2
         // Toast.makeText(this, requestCode + "_" + resultCode, Toast.LENGTH_SHORT).show();
         if (resultCode == 1) {
             Toast.makeText(this, "Photo Taken", Toast.LENGTH_SHORT).show();
-
-            String fileName = data.getStringExtra("FileName");
             PhotoSerial++;
 
+            String fileName = data.getStringExtra("FileName");
             bin.lhwf3bphoto.setText(bin.lhwf3bphoto.getText() + String.valueOf(PhotoSerial) + " - " + fileName + ";\r\n");
         } else {
             Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
-
-
 }
--- app/src/main/java/com/irfansyed/umeedenau/validation/Form5SectionB.java
+++ app/src/main/java/com/irfansyed/umeedenau/validation/Form5SectionB.java
@@ -2,9 +2,6 @@ package com.irfansyed.umeedenau.validation;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
@@ -14,9 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.irfansyed.umeedenau.validation.databinding.Form5sectionbBinding;
-import com.validatorcrawler.aliazaz.Clear;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -36,171 +30,48 @@ public class Form5SectionB extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bin = DataBindingUtil.setContentView(this, R.layout.form5sectionb);
-
-        setupSkips();
-
         PhotoSerial = 0;
 
+
         bin.btnNext.setOnClickListener(this);
-        bin.btnSnap.setOnClickListener(this);
-    }
+
+        /*bin.lhwf5b11.setOnCheckedChangeListener(this);
+        bin.lhwf5b12.setOnCheckedChangeListener(this);*/
 
 
-    private void setupSkips() {
-
-        bin.lhwf5b1.setOnCheckedChangeListener(((radioGroup, i) -> {
-
-            if (bin.lhwf5b12.isChecked()) {
-
-                bin.cvlhwf5b2.setVisibility(View.GONE);
-                bin.cvlhwf5b3.setVisibility(View.GONE);
-                bin.cvlhwf5b4.setVisibility(View.GONE);
-                bin.cvlhwf5b5a.setVisibility(View.GONE);
-                bin.cvlhwf5b5b.setVisibility(View.GONE);
-                bin.cvlhwf5b5c.setVisibility(View.GONE);
-                bin.cvlhwf5b5a1.setVisibility(View.GONE);
-                bin.cvlhwf5b5b1.setVisibility(View.GONE);
-                bin.cvlhwf5b6.setVisibility(View.GONE);
-                bin.cvlhwf5b7.setVisibility(View.GONE);
-
-                Clear.clearAllFields(bin.cvlhwf5b2);
-                Clear.clearAllFields(bin.cvlhwf5b3);
-                Clear.clearAllFields(bin.cvlhwf5b4);
-                Clear.clearAllFields(bin.cvlhwf5b5a);
-                Clear.clearAllFields(bin.cvlhwf5b5b);
-                Clear.clearAllFields(bin.cvlhwf5b5c);
-                Clear.clearAllFields(bin.cvlhwf5b5a1);
-                Clear.clearAllFields(bin.cvlhwf5b5b1);
-                Clear.clearAllFields(bin.cvlhwf5b6);
-                Clear.clearAllFields(bin.cvlhwf5b7);
-
-            } else if (bin.lhwf5b11.isChecked()) {
-
-                bin.cvlhwf5b2.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b3.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b4.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b5a.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b5b.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b5c.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b5a1.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b5b1.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b6.setVisibility(View.VISIBLE);
-                bin.cvlhwf5b7.setVisibility(View.VISIBLE);
-
-            }
-
-        }));
-
-
-        bin.lhwf5b5a1.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                if (bin.lhwf5b5a1.getText().toString().equals("0")) {
-                    bin.cvlhwf5b5b1.setVisibility(View.VISIBLE);
-
-                    Clear.clearAllFields(bin.cvlhwf5b6);
-                    Clear.clearAllFields(bin.cvlhwf5b7);
-
-                    bin.cvlhwf5b6.setVisibility(View.GONE);
-                    bin.cvlhwf5b7.setVisibility(View.GONE);
-
-                } else {
-                    bin.cvlhwf5b5b1.setVisibility(View.GONE);
-                    Clear.clearAllFields(bin.cvlhwf5b5b1);
-
-
-                    if (bin.lhwf5b11.isChecked()) {
-                        bin.cvlhwf5b6.setVisibility(View.VISIBLE);
-                        bin.cvlhwf5b7.setVisibility(View.VISIBLE);
-                    } else {
-                        bin.cvlhwf5b6.setVisibility(View.GONE);
-                        bin.cvlhwf5b7.setVisibility(View.GONE);
-                    }
-
-                }
-            }
-        });
-
-
-        bin.lhwf5b5b1.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (bin.lhwf5b5b196.isChecked()) {
-                bin.lhwf5b5b196x.setVisibility(View.VISIBLE);
-            } else {
-                bin.lhwf5b5b196x.setVisibility(View.GONE);
-                Clear.clearAllFields(bin.lhwf5b5b196x);
-            }
-
-        }));
-
-
-        bin.lhwf5b796.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b == true) {
-                    bin.lhwf5b796x.setVisibility(View.VISIBLE);
-                } else {
-                    bin.lhwf5b796x.setVisibility(View.GONE);
-                    Clear.clearAllFields(bin.lhwf5b796x);
-                }
-            }
-        });
 
     }
 
 
     @Override
     public void onClick(View view) {
-
         if (view.getId() == R.id.btn_snap) {
-
             Intent intent = new Intent(this, TakePhoto.class);
 
             //   intent.putExtra("picID", "901001" + "_" + "A-0001-001" + "_" + PhotoSerial + "_");
             //   intent.putExtra("childName", "Hassan");
+
 // TODO: add identification information
-            intent.putExtra("picID", Global.LhwSection_id + "_" + PhotoSerial);
-            intent.putExtra("childName", "WSG SR");
-            intent.putExtra("picView", "Sect_WSG_SR".toUpperCase());
+            intent.putExtra("picID", Global.LhwHH_id + "_" + Global.LhwSection_id + "_" + PhotoSerial);
+            intent.putExtra("childName", "WOMEN SUPPORT GROUP (WSG)");
+
+
+            intent.putExtra("picView", "Sect_6B".toUpperCase());
             startActivityForResult(intent, 1); // Activity is started with requestCode 1 = Front
 
         }
-
-
-        if (view.getId() == R.id.btnNext) {
-
-            if (!formValidation()) {
-                return;
-            }
-
-            insert_data();
-            Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
-
-            finish();
+        if (!formValidation()) {
+            return;
         }
+
+        insert_data();
+        Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 
     private boolean formValidation() {
-
-        if (!ValidatorClass.EmptyCheckingContainer(this, bin.SectionB)) {
-            return false;
-        }
-
-
-        if (!bin.lhwf5bphoto.getText().toString().equals("")) {
-
-        } else {
-            Toast.makeText(this, "Please take photo", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-
-        return true;
+        return ValidatorClass.EmptyCheckingContainer(this, bin.SectionB);
     }
 
 
@@ -222,42 +93,20 @@ public class Form5SectionB extends AppCompatActivity implements View.OnClickList
     }
 
 
-    /*@Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (buttonView.getId() == R.id.lhwf5b1_1 || buttonView.getId() == R.id.lhwf5b1_2) {
-
-            if (bin.lhwf5b12.isChecked()) {
-
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b2);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b3);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b4);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b5a);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b5b);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b5c);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b6);
-                ClearAllcontrol.ClearAll(bin.LvLhwf5b7);
-
-            }
-        }
-    }*/
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
         // check if the request code is same as what is passed  here it is 2
         // Toast.makeText(this, requestCode + "_" + resultCode, Toast.LENGTH_SHORT).show();
         if (resultCode == 1) {
             Toast.makeText(this, "Photo Taken", Toast.LENGTH_SHORT).show();
-
-            String fileName = data.getStringExtra("FileName");
             PhotoSerial++;
 
+            String fileName = data.getStringExtra("FileName");
             bin.lhwf5bphoto.setText(bin.lhwf5bphoto.getText() + String.valueOf(PhotoSerial) + " - " + fileName + ";\r\n");
         } else {
             Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
-
 }
